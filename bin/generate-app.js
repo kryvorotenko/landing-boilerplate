db06d130c20364ab07e51eb52fe408cc9c50b1ed
kryvorotenko <ilakrivorotenko@gmail.com>
2025-04-27 14:12:15 +0200

    [RELEASE] Boilerplate is ready to use

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -3,10 +3,13 @@
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs').promises;
+const glob = require('glob');
+
+const GIT_REPO = "https://github.com/kryvorotenko/landing-boilerplate";
 
 if (process.argv.length < 3) {
     console.log('You have to provide a name for your app.');
-    console.log('For example :');
+    console.log('For example:');
     console.log('    npx landing-boilerplate my-app');
     process.exit(1);
 }
@@ -14,50 +17,91 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = "https://github.com/kryvorotenko/landing-boilerplate";
+
+async function createProjectDirectory() {
+    try {
+        await fs.mkdir(projectPath);
+        console.log(`Created project directory: ${projectPath}`);
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.log(`The directory ${projectName} already exists. Please choose a different name.`);
+            process.exit(1);
+        } else {
+            throw err;
+        }
+    }
+}
+
+function executeCommand(command, options = {}) {
+    try {
+        execSync(command, { stdio: 'inherit', ...options });
+    } catch (error) {
+        console.error(`Error executing command: ${command}`);
+        throw error;
+    }
+}
+
+async function cloneRepo() {
+    console.log('Downloading files...');
+    executeCommand(`git clone --depth 1 ${GIT_REPO} ${projectPath}`);
+}
+
+async function installDependencies() {
+    console.log('Installing dependencies...');
+    executeCommand('npm install');
+}
+
+async function removeUnnecessaryFiles() {
+    console.log('Removing unnecessary files...');
+    executeCommand('npx rimraf ./.git');
+}
+
+async function removeBinDirectory() {
+    const binPath = path.join(projectPath, 'bin');
+    try {
+        await fs.rmdir(binPath, { recursive: true });
+        console.log('Removed bin directory.');
+    } catch (err) {
+        console.log('No bin directory to remove.');
+    }
+}
+
+async function updatePackageJson() {
+    const packageJsonPath = path.join(projectPath, 'package.json');
+    const packageJson = require(packageJsonPath);
+    packageJson.name = projectName;
+    delete packageJson.bin;
+    await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+}
+
+async function deleteGitKeepFiles() {
+    glob(path.join(projectPath, '**/.gitkeep'), { ignore: '**/node_modules/**' }, async (err, files) => {
+        if (err) {
+            console.error('Error searching for .gitkeep files:', err);
+            return;
+        }
+
+        for (const file of files) {
+            try {
+                await fs.unlink(file);
+                console.log(`Deleted .gitkeep file: ${file}`);
+            } catch (error) {
+                console.error(`Error deleting ${file}:`, error);
+            }
+        }
+    });
+}
 
 async function createProject() {
     try {
-        console.log(`Creating project directory: ${projectPath}`);
-
-        try {
-            await fs.mkdir(projectPath);
-        } catch (err) {
-            if (err.code === 'EEXIST') {
-                console.log(`The directory ${projectName} already exists. Please choose a different name.`);
-                process.exit(1);
-            } else {
-                throw err;
-            }
-        }
-
-        console.log('Downloading files...');
-        execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`, { stdio: 'inherit' });
-
+        await createProjectDirectory();
+        await cloneRepo();
         process.chdir(projectPath);
-
-        console.log('Installing dependencies...');
-        execSync('npm install', { stdio: 'inherit' });
-
-        console.log('Removing unnecessary files...');
-        execSync('npx rimraf ./.git', { stdio: 'inherit' });
-
-        const binPath = path.join(projectPath, 'bin');
-        try {
-            await fs.rmdir(binPath, { recursive: true });
-            console.log('Removed bin directory.');
-        } catch (err) {
-            console.log('No bin directory to remove.');
-        }
-
-        const packageJsonPath = path.join(projectPath, 'package.json');
-        const packageJson = require(packageJsonPath);
-
-        packageJson.name = projectName;
-        delete packageJson.bin;
-
-        await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
+        await installDependencies();
+        await removeUnnecessaryFiles();
+        await removeBinDirectory();
+        await updatePackageJson();
+        await deleteGitKeepFiles();
         console.log('Project setup complete. You are ready to go!');
     } catch (error) {
         console.error('An error occurred during the setup:', error.message);
